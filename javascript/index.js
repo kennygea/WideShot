5be0d4f118f4c11e651b1691bd72fffd2cc58455
kennygea <kennygea@mit.edu>
2016-04-21 23:37:56 -0400

    working filter

--- javascript/index.js
+++ javascript/index.js
@@ -12,6 +12,10 @@ $(document).ready(function() {
 		{ title: 'Directors' },
 		{ title: 'Editors' },
 		{ title: 'Actors' }];
+		
+	$('#createButton').on('click', function() {
+		console.log($('.ui.form'));
+	});
 			
 		
 	$('.ui.search')
@@ -23,7 +27,31 @@ $(document).ready(function() {
 	$('.ui.dropdown')
 	  .dropdown({
 		// you can use any ui transition
-		transition: 'drop'
+		transition: 'drop',
+		onChange: function(values, text, $selectedItem) {
+			  console.log(values);
+			  var allProjects = $('.projectInfo');
+			  allProjects.each(function(){
+				  tags = []
+				  $(this).find('.ui.label.tags').each(function(index, obj) {
+						tags.push($(this).text())
+					})
+				  var isSet = values.every(function(val){
+					  if (values.len === 0) {
+						  return true
+					  }
+					  return tags.indexOf(val) >= 0;
+				  });
+				  
+				  if (!isSet) {
+					  $(this).css({'display': 'none'})
+				  }
+				  else {
+					  $(this).css({'display': ''})
+				  }
+			  })
+		  }
+
 	  });
 	  
 	$('#createProject').click(function() {
