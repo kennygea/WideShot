2344523973126620ce390d8a7c2ab3bacadc694c
unknown <kennygea@mit.edu>
2016-05-02 08:34:57 -0700

    more cookies

--- javascript/project.js
+++ javascript/project.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
 		var d = new Date();
 		d.setTime(d.getTime() + (exdays*24*60*60*1000));
 		var expires = "expires="+d.toUTCString();
-		document.cookie = cname + "=" + cvalue + "; " + expires + ';path=../';
+		document.cookie = cname + "=" + cvalue + "; " + expires + ';path=/';
 	}
 
 	function getCookie(cname) {
